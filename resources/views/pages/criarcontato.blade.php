d420c061858b3f5fbe740b1c3b99ee316ac903ad
andresconobre <andre.pires.nobre@hotmail.com>
2021-11-16 21:26:28 -0300

    added bootstrap css and js; changed button and table styles

--- resources/views/pages/criarcontato.blade.php
+++ resources/views/pages/criarcontato.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    
-    <h1 style="color: #FFFFFF;">{{$variavel}}</h1>
-    <p style="color: #FFFFFF;">Projeto BO2 - Criar contato</p>
+
+<h1 style="color: #FFFFFF;">{{$variavel}}</h1>
+<p style="color: #FFFFFF;">Projeto BO2 - Criar contato</p>
 @endsection('content')
