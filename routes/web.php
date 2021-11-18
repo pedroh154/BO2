bf2ee3aba4958aab4a05833c18948faced44729e
pedroh154 <pedrohbr154@gmail.com>
2021-11-18 15:45:20 -0300

    fix for /home

--- routes/web.php
+++ routes/web.php
@@ -33,5 +33,4 @@ Route::get('/forgotpassword', 'App\Http\Controllers\PagesController@forgotpasswo
 
 
 Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
