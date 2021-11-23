71835cb1643a955636f112fcfdb809b3439b57b6
andresconobre <andre.pires.nobre@hotmail.com>
2021-11-23 20:20:55 -0300

    for each test

--- resources/views/contatos/index.blade.php
+++ resources/views/contatos/index.blade.php
@@ -11,29 +11,39 @@
 <!--<p style="color: #FFFFFF;"></p>-->
 
 <!--<table class="table table-striped table-hover table-bordered caption-top" >-->
-<table class="table table-striped table-hover ">
-    <thead>
-        <tr class="table-active">
-            <th scope="col" style="text-align: left;">Contatos</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
 
-            <ul style="list-style-type: none;">
-                <td>
-                    @if(count($data) > 0)
-                    @foreach($data as $o)
-                    <li style="list-style-type: none;">{{$o}}</li>
-                    @endforeach
-                </td>
+<div id="content-wrapper" class="d-flex flex-column" style="background-color: #f4f5f8;">
+    <div class="container-fluid">
+        <h1>Contatos</h1>
+        @csrf
+        @method('DELETE')
+        <table class="table table-striped table-hover table-bordered table-sm">
+            <thead>
+                <tr style="text-align: center;">
+                    <th style="width: 150px;" scope="col">Nome</th>
+                    <th style="width: 70px;" scope="col">Telefone</th>
+                    <th style="width: 100px;" scope="col">Endereço</th>
+                    <th style="width: 75px;" scope="col">Descrição</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($contato as $contato)
+                @if($contato->type == 'Nome')
+                <tr>
+                    <td style="text-align: left;">{{ $contato->nome }}</td>
+                    </td>
+
+
+                </tr>
                 @endif
-            </ul>
+                @endforeach
 
-        </tr>
-    </tbody>
 
-</table>
+            </tbody>
+
+        </table>
+    </div>
+</div>
 <!--<a href="/novocontato" >
     <input type="submit" value="" class="btn btn-outline-dark" style="color: #FFFFFF;">-->
 
--- resources/views/despesas/index.blade.php
+++ resources/views/despesas/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+
+
+
 <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
 
 <!--<div class="btn-group" role="group" aria-label="Basic example">
@@ -8,7 +11,38 @@
             <a href="/contatos" style="color: #FFFFFF;"><button type="button" class="btn btn-primary">Contatos</button></a>
         </div>-->
 <h1></h1>
-<p>DESPESAS AQUI</p>
+<div id="content-wrapper" class="d-flex flex-column" style="background-color: #f4f5f8;">
+    <div class="container-fluid">
+        <h1>Contatos</h1>
+        @csrf
+        @method('DELETE')
+        <table class="table table-striped table-hover table-bordered table-sm">
+            <thead>
+                <tr style="text-align: center;">
+                    <th style="width: 150px;" scope="col">Nome</th>
+                    <th style="width: 70px;" scope="col">Telefone</th>
+                    <th style="width: 100px;" scope="col">Endereço</th>
+                    <th style="width: 75px;" scope="col">Descrição</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($listDespesas as $despesa)
+
+                <tr>
+                    <td style="text-align: left;">{{ $despesa->categoria }}</td>
+                    </td>
+
+
+                </tr>
+
+                @endforeach
+
+
+            </tbody>
+
+        </table>
+    </div>
+</div>
 <footer class="container">
   <!--  <a href="https://sistema.ssw.inf.br/bin/ssw0422" target="_blank" style="color: #FFFFFF;"><button type="button" class="btn btn-primary">SSW</button></a>
     <a href="/logout" style="color: #FFFFFF;"><button type="button" class="btn btn-primary">Logout</button></a>
