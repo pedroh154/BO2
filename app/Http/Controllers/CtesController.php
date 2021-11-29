e5e999f21eb920c2fc92b7af01201835c2e2d4f1
pedroh154 <pedrohbr154@gmail.com>
2021-11-29 20:19:39 -0300

    username now shows on layout/app

--- app/Http/Controllers/CtesController.php
+++ app/Http/Controllers/CtesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Cte;
 use App\Models\Cliente;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Cidade;
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -168,7 +168,7 @@
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item dropdown no-arrow">
                         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span class="mr-2 d-none d-lg-inline text-gray-600 small">Acir Pires Nobre</span>
+                            <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ $username }}</span>
                             <img class="img-profile rounded-circle" src="https://source.unsplash.com/Qb7D1xw28Co/150x150">
                         </a>
                         <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
